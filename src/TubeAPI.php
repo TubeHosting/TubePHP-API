a83bb50b847352debcb5ceb47ddb3892c230c22c
Julian Gransee <julian@gransee.me>
2022-01-01 01:56:27 +0100

    implemented request exception in request function

--- src/TubeAPI.php
+++ src/TubeAPI.php
@@ -2,13 +2,14 @@
 
 namespace TubeAPI;
 
+use TubeAPI\Exceptions;
+use TubeAPI\Exceptions\RequestException;
+
+require_once __DIR__ . '/Exceptions/RequestException.php';
+
 class TubeAPI
 {
-    private $apiUrl = 'https://api.tube-hosting.com';
-
-    private $userAgent = 'API';
-
-    public static $token;
+    public static string $token;
 
     /**
      * @param string $method
@@ -16,30 +17,31 @@ class TubeAPI
      * @param array|null $data
      * @param string|null $bearer
      * @param array|null $customHeaders
-     * @return bool|string
-     * @throws \Exception
+     * @return string
+     * @throws RequestException
      */
-    public static function request(string $method, string $endpoint, ?array $data = array('null'), ?string $bearer = 'null', ?array $customHeaders = null): bool|string
+    public static function request(string $method, string $endpoint, ?array $data = array('null'), ?string $bearer = 'null', ?array $customHeaders = null): string
     {
 
         $curl = curl_init();
 
+        //set necessary http headers
         $headers = array(
             'Connection: keep-alive',
             'Accept: */*',
             'Access-Control-Request-Headers: authorization,content-type',
-            'User-Agent: API',
-            'Accept-Language: en-US,en;q=0.9',
+            'User-Agent: PHP-Client v0.2',
             'Content-Type: application/json; charset=utf-8',
             'Authorization: Bearer ' . $bearer
         );
         if ($customHeaders !== null) $headers = array_merge($headers, $customHeaders);
 
+        //set options
         curl_setopt_array($curl, [
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_URL => 'https://api.tube-hosting.com' . $endpoint,
-            CURLOPT_CUSTOMREQUEST => $method,
-            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_CUSTOMREQUEST => $method, //set http method
+            CURLOPT_POSTFIELDS => json_encode($data), //add json data
             CURLOPT_ENCODING => 'gzip, deflate'
         ]);
 
@@ -48,14 +50,34 @@ class TubeAPI
         $data = curl_exec($curl);
         $http_code = ((int)curl_getinfo($curl)['http_code']);
 
-        if (!is_array(curl_getinfo($curl)) || $http_code !== 200){ //handle unexpected behavior
-            if ($http_code === 0) $errormsg = "Cannot create connection!";
-            else {
-                $errormsg = "Request failed: ". $http_code;
-            }
+
+        if (!$data || !is_array(curl_getinfo($curl)) || $http_code !== 200){ //handle unexpected behavior
+
+            //failed requests
+            if (!$data) $errormsg = "Request Failed!";
+            else if ($http_code === 0) $errormsg = "Cannot create connection!";
+            //status code != 200/0
+            else $errormsg = "Request failed: ". $http_code;
+
+            //if something is getting returned, put it in the error message
             if((string)$data !== "") $errormsg = $errormsg . " -  ".$data;
-            throw new \Exception($errormsg);
+
+            //prepare for throwing exception
+            if (!is_array(curl_getinfo($curl))){
+                $curl_getInfo = null;
+                $http_code = null;
+            }else{
+                $curl_getInfo = curl_getinfo($curl);
+                $http_code =  (int)curl_getinfo($curl)['http_code'];
+            }
+
+            if (!$data || (string) $data === ""){
+                $data = null;
+            }
+
+            throw new RequestException($curl_getInfo, "https://api.tube-hosting.com" . $endpoint, $data, $http_code, $errormsg, 0);
         }
+
         curl_close($curl);
 
         return $data;
